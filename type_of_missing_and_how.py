6dcef82ef908fe89db9ee94657726fc5fd33be37
Nguyen Nguyen <nguyenuyennguyen03@gmail.com>
2026-01-09 16:40:45 -0800

    add the missing how manya and what type of missingness function

---
+++ type_of_missing_and_how.py
@@ -0,0 +1,15 @@
+def missing_how_type_(df):
+    """
+    Print out the number of missing data in the data set, and also identify and print out the type of missigness.
+    There are 3 types of missingness: Missing completely at random (MCAR), 
+    Missing at random (MAR), Missing Not at random (MNAR)
+
+    Parameters
+    ----------
+    df(pd.DataFrames) : A pandas dataframe to analyze missingness.
+
+    Returns
+    -------
+    None
+
+    """
