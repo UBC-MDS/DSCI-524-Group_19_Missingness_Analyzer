d53c53f0aed32fdb049690a2e7bf28f05434e85f
Rocco Lee <roccolee18@gmail.com>
2026-01-14 17:03:21 -0800

    Progress on unit testing

--- tests/test_suggest_imputation.py
+++ tests/test_suggest_imputation.py
@@ -1,1 +1,14 @@
+import os
+import sys
 import pytest
+import pandas as pd
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from src.missingness_analyzer.suggest_imputation import suggest_imputation
+
+def test_missingness_identification():
+    """Testing to see if fucntion accurately identifies the amount of missingness in the dataset"""
+
+    df = pd.read_csv("tests\synthetic_dataset.csv")
+    result = suggest_imputation(df)
+    assert result["missingness_amount"] == 30.8
