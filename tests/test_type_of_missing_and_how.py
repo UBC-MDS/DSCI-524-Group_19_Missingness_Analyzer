1fa7ff521d4dbbf33131d970e5d7a2f719534c70
Nguyen Nguyen <nguyenuyennguyen03@gmail.com>
2026-01-15 13:25:40 -0800

    creating test

---
+++ tests/test_type_of_missing_and_how.py
@@ -0,0 +1,10 @@
+import os
+import sys
+import pytest
+import pandas as pd
+import numpy as np
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from src.missingness_analyzer.type_of_missing_and_how import type_of_missing_and_how
+
+def 
