8a1401637dd0622bd0126612604bdff2ce6e704b
Nguyen Nguyen <nguyenuyennguyen03@gmail.com>
2026-01-16 18:15:02 -0800

    finish write missing function

--- src/missingness_analyzer/type_of_missing_and_how.py
+++ src/missingness_analyzer/type_of_missing_and_how.py
@@ -12,6 +12,42 @@ def missing_how_type_(df):
     -------
     None
 
-    
-
     """
+
+    results = []
+
+    if df.isna().sum().sum() == 0:
+        print("The data set have no missing values.")
+        return
+
+    for target in df.columns:
+
+        if df[target].isna().sum() == 0:
+            results.append({"target": target,
+                            "MCAR": "No Missing Value"})
+            continue
+
+        for other_target in df.columns:
+            if target == other_target:
+                continue
+
+            missing = df[target].isna()
+            table = pd.crosstab(missing, df[other_target])
+
+            if table.shape[1] < 2:
+                continue
+
+            chi2, p, dof, exp = chi2_contingency(table)
+
+            results.append({
+                "target": target,
+                "other_target": other_target,
+                "p_value": p,
+                "MCAR": p > alpha
+            })
+    
+    print("\nColumns with True value is Missing Completely at Random (MCAR)")
+    print("Columns with False value are either Missing at Random (MAR) or Missing Not at Random (MNAR)")
+    print("Since MAR and MNAR cannot be tested statistically and formally, additional domain expertise is needed for further investigation")
+
+    return pd.DataFrame(results).groupby("target")[["MCAR"]].all()   
