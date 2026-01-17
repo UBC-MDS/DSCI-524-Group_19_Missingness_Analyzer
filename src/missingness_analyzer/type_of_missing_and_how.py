0f7a1086aac2f23e843e6ab51ca7148be0dbf5f1
Nguyen Nguyen <nguyenuyennguyen03@gmail.com>
2026-01-16 20:18:35 -0800

    add more defending mechanism in the function code

--- src/missingness_analyzer/type_of_missing_and_how.py
+++ src/missingness_analyzer/type_of_missing_and_how.py
@@ -1,4 +1,8 @@
-def missing_how_type_(df):
+import pandas as pd
+import numpy as np
+from scipy.stats import chi2_contingency
+
+def missing_how_type_(df, alpha=0.05):
     """
     Print out a report that contain the number of missing data in the data set, and also identify and print out the type of missigness.
     There are 3 types of missingness: Missing completely at random (MCAR), 
@@ -10,14 +14,19 @@ def missing_how_type_(df):
 
     Returns
     -------
-    None
+    A data frame that show boolean value of which columns are MCAR or MAR and MNAR
 
     """
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("Your df is not a pandas DataFrame.")
+    
+    if not (0 < alpha < 1):
+        raise ValueError("Your alpha value must be between 0 and 1.")
 
     results = []
 
     if df.isna().sum().sum() == 0:
-        print("The data set have no missing values.")
+        print("The data set has no missing values.")
         return
 
     for target in df.columns:
@@ -36,16 +45,23 @@ def missing_how_type_(df):
 
             if table.shape[1] < 2:
                 continue
-
-            chi2, p, dof, exp = chi2_contingency(table)
+            
+            try:
+                p = chi2_contingency(table)[1]
+            except ValueError:
+                raise ValueError(
+                    f"Chi-square test failed for column {target} and {other_target}"
+                )
 
             results.append({
                 "target": target,
                 "other_target": other_target,
-                "p_value": p,
+                "p.value": p,
                 "MCAR": p > alpha
             })
     
+    print(f"This data frame have {df.isna().sum().sum()} missing values, below is the number of missing values for each column:")
+    print(df.isna().sum())
     print("\nColumns with True value is Missing Completely at Random (MCAR)")
     print("Columns with False value are either Missing at Random (MAR) or Missing Not at Random (MNAR)")
     print("Since MAR and MNAR cannot be tested statistically and formally, additional domain expertise is needed for further investigation")
